ac6d92ce942338dc9e61898a800754943c76d5d2
Valentin Nikolov <v.nikolov@vanlanschotkempen.com>
2023-11-18 19:31:58 +0100

    The most significant changes involve the addition of a new `MathSkill.cs` file, which contains a class with several mathematical operations, and the modification of the `Program.cs` file, which now includes code for setting up the Semantic Kernel, reading a plan from a new `Plan.json` file, and executing this plan. The `SK-Planner.csproj` file was also updated to include several new package references and to always copy the `Plan.json` file to the output directory.
    1. A new `MathSkill.cs` file was added. This file contains a `MathSkill` class with several static methods for mathematical operations. These methods are decorated with `SKFunction` and `Description` attributes, providing metadata for the Semantic Kernel.
    
    2. A new `Plan.json` file was added. This file defines a plan for a sequence of operations to be performed by the Semantic Kernel, involving multiplication and subtraction.
    
    3. The `Program.cs` file was modified. The `Main` method is now asynchronous and includes code for setting up the Semantic Kernel, importing the `MathSkill` functions, creating a `SequentialPlanner`, reading a plan from the `Plan.json` file, executing the plan, and printing the result.
    
    4. The `SK-Planner.csproj` file was updated. New package references were added, including `Microsoft.SemanticKernel.Connectors.AI.OpenAI`, `Microsoft.SemanticKernel.Core`, and `Microsoft.SemanticKernel.Planners.Core`. The `Plan.json` file was also added to the project with instructions to always copy it to the output directory.

--- SK-Planner/Program.cs
+++ SK-Planner/Program.cs
@@ -1,28 +1,53 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
+using Microsoft.SemanticKernel.Memory;
+using Microsoft.SemanticKernel.Planners;
+using System.Text.Json;
 
 namespace SK_Planner
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddUserSecrets<Program>(); // Program is your console application class
 
             IConfigurationRoot configuration = builder.Build();
-            var endpoint = configuration["Endpoint"];
-            var key = configuration["Key"];
-            var completionModel = configuration["CompletionModel"];
-            var embeddingModel = configuration["EmbeddingModel"];
+            var endpoint = configuration["Endpoint"]!;
+            var key = configuration["Key"]!;
+            var completionModel = configuration["CompletionModel"]!;
+            var embeddingModel = configuration["EmbeddingModel"]!;
 
-            Console.WriteLine($"Endpoint: {endpoint}");
-            Console.WriteLine($"Key: {key}");
-            Console.WriteLine($"CompletionModel: {completionModel}");
-            Console.WriteLine($"EmbeddingModel: {embeddingModel}");
+            IKernel kernel = new KernelBuilder()
+                //.WithLoggerFactory(sp.GetRequiredService<ILoggerFactory>())
+                //.WithMemory(sp.GetRequiredService<ISemanticTextMemory>())
+                .WithAzureOpenAIChatCompletionService(deploymentName: completionModel, endpoint: endpoint, apiKey: key)
+                .WithAzureOpenAITextEmbeddingGenerationService(deploymentName: embeddingModel, endpoint: endpoint, apiKey: key)
+                .Build();
 
-            Console.WriteLine("Hello, World!");
+            var mathPlugin = kernel.ImportFunctions(new MathSkill(), "MathPlugin");
+
+            // Create planner
+            var planner = new SequentialPlanner(kernel);
+
+            var ask = "If my investment of 2130.23 dollars increased by 23%, how much would I have after I spent $5 on a latte?";
+            //var plan = await planner.CreatePlanAsync(ask);
+
+            //Console.WriteLine("Plan:\n");
+            //Console.WriteLine(JsonSerializer.Serialize(plan, new JsonSerializerOptions { WriteIndented = true }));
+
+            var planJson = await File.ReadAllTextAsync("Plan.json");
+            var planFromJson = kernel.ImportPlanFromJson(planJson);
+
+            // Execute the plan
+            var result = await kernel.RunAsync(planFromJson);
+
+            Console.WriteLine("Plan results:");
+            Console.WriteLine(result.GetValue<string>()!.Trim());
         }
     }
 }
