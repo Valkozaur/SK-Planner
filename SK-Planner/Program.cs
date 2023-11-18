c1aa9a7746eb26ee0b12ae40300f9aec51963b8e
Valentin Nikolov <v.nikolov@vanlanschotkempen.com>
2023-11-18 18:54:45 +0100

    Get user secrets

--- SK-Planner/Program.cs
+++ SK-Planner/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace SK_Planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddUserSecrets<Program>(); // Program is your console application class
+
+            IConfigurationRoot configuration = builder.Build();
+            var endpoint = configuration["Endpoint"];
+            var key = configuration["Key"];
+            var completionModel = configuration["CompletionModel"];
+            var embeddingModel = configuration["EmbeddingModel"];
+
+            Console.WriteLine($"Endpoint: {endpoint}");
+            Console.WriteLine($"Key: {key}");
+            Console.WriteLine($"CompletionModel: {completionModel}");
+            Console.WriteLine($"EmbeddingModel: {embeddingModel}");
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
